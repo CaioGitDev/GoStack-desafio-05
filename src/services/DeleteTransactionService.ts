0d4fcc70c4a12dbd176ae7b69f33aeb16f28e195
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2020-09-07 21:22:50 +0100

    feat: implement delete transactions

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+    const transactionToRemove = await transactionRepository.findOne(id);
+
+    if (!transactionToRemove) {
+      throw new AppError('Bad request');
+    }
+
+    await transactionRepository
+      .remove(transactionToRemove)
+      .then(() => console.log('Transaction Removed'));
   }
 }
 
